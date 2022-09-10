f425f2d8f543fba39581936c78c7c2aaf94274d7
mirzavic4869 <avicenamirzam@gmail.com>
2022-09-11 01:27:28 +0700

    create 4 langkah page

---
+++ src/components/Langkah.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Registration from "../image/registration.jpg";
+import Design from "../image/design.jpg";
+
+const Langkah = () => {
+	return (
+		<div className="container langkah pb-5">
+			<h1 className="title text-center">4 Langkah Menuju Desain Impianmu</h1>
+			<Carousel fade>
+				<Carousel.Item>
+					<img className="d-block w-100 rounded-4" src={Registration} alt="First slide" />
+					<Carousel.Caption>
+						<h3>Registration</h3>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img className="d-block w-100 rounded-4" src={Design} alt="Second slide" />
+					<Carousel.Caption>
+						<h3>Design Your Home</h3>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img className="d-block w-100 rounded-4" src={Registration} alt="Third slide" />
+					<Carousel.Caption>
+						<h3>Registration</h3>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img className="d-block w-100 rounded-4" src={Design} alt="Fourth slide" />
+					<Carousel.Caption>
+						<h3>Design Your Home</h3>
+					</Carousel.Caption>
+				</Carousel.Item>
+			</Carousel>
+		</div>
+	);
+};
+
+export default Langkah;
