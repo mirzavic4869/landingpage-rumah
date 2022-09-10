884b75a1d644b2f91fbaef2a41fe933388adfd98
mirzavic4869 <avicenamirzam@gmail.com>
2022-09-10 20:53:49 +0700

    responsive home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -8,22 +7,15 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 const Home = () => {
 	return (
 		<>
-			<style type="text/css">
-				{`
-    .border {
-      font-size: 16px
-      color: #EAE3D2;  
-    }
-    `}
-			</style>
-			<Navbar collapseOnSelect expand="lg">
+			<div className="home"></div>
+			<Navbar collapseOnSelect expand="lg" className="bg-transparent ">
 				<Container>
-					<Navbar.Brand href="#home" className="border rounded-pill py-2 px-4">
+					<Navbar.Brand href="#home" className="border rounded-pill py-2 px-4 navbar-brand">
 						Proyek Saya
 					</Navbar.Brand>
 					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 					<Navbar.Collapse id="responsive-navbar-nav">
-						<Nav className="me-auto">
+						<Nav className="mx-auto nav-link">
 							<NavDropdown title="Layanan" id="collasible-nav-dropdown">
 								<NavDropdown.Item href="#action/3.1">Career</NavDropdown.Item>
 							</NavDropdown>
@@ -38,9 +30,7 @@ const Home = () => {
 						</Nav>
 						<Nav>
 							<NavDropdown title="Akun" id="collasible-nav-dropdown">
-								<NavDropdown.Item href="#action/3.1">
-									Profil Saya
-								</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.1">Profil Saya</NavDropdown.Item>
 								<NavDropdown.Item href="#action/3.1">Log Out</NavDropdown.Item>
 							</NavDropdown>
 						</Nav>
@@ -49,12 +39,44 @@ const Home = () => {
 			</Navbar>
 			<div className="p-5 mb-4 bg-light rounded-bottom jumbotron">
 				<div className="container-fluid py-5">
-					<h1 className="display-5 fw-bold">Custom jumbotron</h1>
-					<p className="col-md-8 fs-4">
-						Using a series of utilities, you can create this jumbotron, just
-						like the one in previous versions of Bootstrap. Check out the
-						examples below for how you can remix and restyle it to your liking.
-					</p>
+					<h1 className="display-5 text-white fs-32">
+						Hai kamu, <span className="fw-bold fs-40 ">mau desain apa?</span>
+					</h1>
+					<div className="search mx-auto ">Desain Rumah, Interior, Bangunan Lainnya, Struktur Hitung RAB </div>
+					<div className="d-flex justify-content-center">
+						<div className="circle mx-lg-4"></div>
+						<div className="circle mx-lg-4"></div>
+						<div className="circle mx-lg-4"></div>
+						<div className="circle mx-lg-4"></div>
+					</div>
+					<div className="d-flex justify-content-center text-center">
+						<div className="pill mx-lg-4">Rumah</div>
+						<div className="pill mx-lg-4">Interior</div>
+						<div className="pill mx-lg-4">Bangunan Lain</div>
+						<div className="pill mx-lg-4">Hitung RAB</div>
+					</div>
+					<div className="d-flex justify-content-center">
+						<div className="circle mx-lg-4"></div>
+						<div className="circle mx-lg-4"></div>
+						<div className="circle mx-lg-4"></div>
+					</div>
+					<div className="d-flex justify-content-center text-center">
+						<div className="pill mx-lg-4">Struktur</div>
+						<div className="pill mx-lg-4">Partner</div>
+						<div className="pill mx-lg-4">Giivepro</div>
+					</div>
+				</div>
+			</div>
+			<div className="panels">
+				<div className="panel container d-flex justify-content-around ">
+					<p>523</p>
+					<p>1025</p>
+					<p>1520</p>
+				</div>
+				<div className="panel-text container d-flex justify-content-around ">
+					<p>Antrian</p>
+					<p>Diproses</p>
+					<p>Selesai</p>
 				</div>
 			</div>
 		</>
