5013198145a88bad7acd9a0267b6a57b4ff371a4
mirzavic4869 <avicenamirzam@gmail.com>
2022-09-11 08:01:19 +0700

    create benefit page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import DesainRumah from "./components/DesainRumah";
 import MengapaAndara from "./components/MengapaAndara";
 import Langkah from "./components/Langkah";
 import TidakMenggunakan from "./components/TidakMenggunakan";
+import Benefits from "./components/Benefits";
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 			<MengapaAndara />
 			<Langkah />
 			<TidakMenggunakan />
+			<Benefits />
 		</div>
 	);
 }
